3c8407a6efe7fec46caf3f32b3134233112f490f
johnnyb88 <johnnybarratt@gmail.com>
2019-10-09 21:49:23 +0000

    implement ajax so new tweet shows on page after submition

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,67 @@
-$(document).ready( () => {
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+$(document).ready(() => {
 
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      data: {
+        format: 'json'
+      }
+    })
+      .done(function (tweets) {
+        renderTweets(tweets);
+      });
+  };
+  loadTweets();
+
+
+  //----ajax post request that post to server with a serialized data ---//
+  const $button = $('#tweetForm');
+  $button.on('submit', function (event) {
+    event.preventDefault();
+    console.log('Button clicked, performing ajax call...');
+
+    let tweetPost = $("#tweetPost");
+    let tweetData = $("tweetForm").serialize();
+
+    if (tweetPost.val().length === 0) {
+      alert("Cannot post empty field");
+    } else if (tweetPost.val().length > 140) {
+      alert("140 character limit, Please edit your post.");
+    } else {
+
+      $.ajax({ url: '/tweets', method: 'POST', data: $(this).serialize()  })
+        .then(function (newTweet) {
+          $('.tweets-container').empty();
+          loadTweets(newTweet);
+        });
+    }
+  });
+  
+  
 
   const renderTweets = function(tweets) {
     const container = $('.tweets-container');
     for (let tweet of tweets) {
       let output = createTweetElement(tweet);
-      container.append(output);
+      container.prepend(output);
     }
+  };
+
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   };
 
   const createTweetElement = function(tweet) {
     return ` <article>
-  <header class="tweetHeader"><img class= "profile" src="${tweet.user.avatars}"><h3>${tweet.user.name}</h3>
-    <h4 class="tagName">${tweet.user.handle}</h4>
+  <header class="tweetHeader"><img class= "profile" src="${escape(tweet.user.avatars)}"><h3>${tweet.user.name}</h3>
+    <h4 class="tagName">${escape(tweet.user.handle)}</h4>
   </header>
-  <p>${tweet.content.text}</p>
+  <p>${escape(tweet.content.text)}</p>
   <footer>
-    <p1>${new Date(tweet.created_at)}</p1>
+    <p1>${escape(new Date(tweet.created_at))}</p1>
     <div class="icons">
       <i class="fa fa-flag"></i>
         <i class="fa fa-heart"></i>
@@ -54,5 +70,7 @@ $(document).ready( () => {
   </footer>
 </article>`;
   };
-  renderTweets(data);
 });
+
+
+
