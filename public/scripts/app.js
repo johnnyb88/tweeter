98c17117f8b7434e6c199f2e8243c2e2c196e2d2
johnnyb88 <johnnybarratt@gmail.com>
2019-10-11 00:32:27 +0000

    added screenshots of app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
 
+
+
+  //------ use jQuery to make a request to /tweets and receive the array of tweets as JSON -----//
   const loadTweets = function() {
     $.ajax({
       method: 'GET',
@@ -18,18 +21,21 @@ $(document).ready(() => {
   //----ajax post request that post to server with a serialized data ---//
   const $button = $('#tweetForm');
   $button.on('submit', function (event) {
+    //----- prevent the default form submission behaviour -----//
     event.preventDefault();
     console.log('Button clicked, performing ajax call...');
 
     let tweetPost = $("#tweetPost");
 
+    //----- Error handling -----//
     if (tweetPost.val().length === 0) {
       $(".errorMessage").text("Error! You cannot post an empty field.").slideDown("slow").addClass(".errorDisplay");
     } else if (tweetPost.val().length > 140) {
       $(".errorMessage").text("Error! You've reached your character limit.").slideDown('slow').addClass(".errorDisplay");
     } else {
       $(".errorMessage").slideUp('fast').removeClass('.errorDisplay');
-      $.ajax({ url: '/tweets', method: 'POST', data: $(this).serialize()  })
+      $.ajax({ url: '/tweets', method: 'POST', data: $(this).serialize() })
+
         .then(function (newTweet) {
           tweetPost.val("");
           $('.tweets-container').empty();
@@ -45,6 +51,8 @@ $(document).ready(() => {
   });
   
 
+
+  //----- take in an array of tweet objects and then appending each one to the #tweets-container -----//
   const renderTweets = function(tweets) {
     const container = $('.tweets-container');
     for (let tweet of tweets) {
@@ -53,12 +61,16 @@ $(document).ready(() => {
     }
   };
 
+  //----- re-encoding text so that unsafe characters are converted into a safe "encoded" representation -----//
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+
+
+  //----- returns tweet article element -----//
   const createTweetElement = function(tweet) {
     return ` <article>
   <header class="tweetHeader"><img class= "profile" src="${escape(tweet.user.avatars)}"><h3>${escape(tweet.user.name)}</h3>
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,3 +9,4 @@ $(document).ready(function() {
     }
   });
 });
+//----- counts characters in the text area -----//
